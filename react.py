0ae1134c0f4590b13ed6748600fe43b21eef7fed
Shivaji Byrapaneni <Shivaji.byrapaneni@gmail.com>
2024-05-27 14:31:16 -0400

    AI ReAct

--- react.py
+++ react.py
@@ -1,71 +1,140 @@
 import logging
 import sys
-from util import get_stock_ticker, get_stock_price, get_recent_stock_news, get_financial_statements, stock_news_search
+from util import extract_docstring_info, construct_format_tool_prompt, CustomTools
 from chains import get_sql_chain
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from langchain_community.llms import Ollama
+from langchain.agents import Tool
+from langchain.agents import AgentExecutor
+from langchain.agents.output_parsers import XMLAgentOutputParser
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(stream=sys.stdout, level=logging.INFO,
                     format='%(asctime)s - %(levelname)s - %(message)s')
 
 
-def analyze_stock(llm, query, db_chain):
-    """Construct a chain that will always invoke all of the tools and use the output to respond to the user query."""
+# def analyze_stock(llm, query, db_chain):
+#     """Construct a chain that will always invoke all of the tools and use the output to respond to the user query."""
 
-    company_name, ticker = get_stock_ticker(query, db_chain)
-    logger.info("\n\n---> Query : \"%s\" \n", query)
-    logger.info("\n---> Company Name : \"%s\" \n", company_name)
-    logger.info("\n---> Ticker : \"%s\" \n", ticker)
-    stock_data = get_stock_price(ticker, history=10)
-    stock_financials = get_financial_statements(ticker)
-    stock_news = get_recent_stock_news(company_name)
+#     company_name, ticker = get_stock_ticker(query, db_chain)
+#     logger.info("\n\n---> Query : \"%s\" \n", query)
+#     logger.info("\n---> Company Name : \"%s\" \n", company_name)
+#     logger.info("\n---> Ticker : \"%s\" \n", ticker)
+#     stock_data = get_stock_price(ticker, history=10)
+#     stock_financials = get_financial_statements(ticker)
+#     stock_news = get_recent_stock_news(company_name)
 
-    analysis_template = """
-            Given detail stock analysis, Use the available data and provide investment recommendation. 
-            The user is fully aware about the investment risk, do not include any kind of warning like 
-            'It is recommended to conduct further research and analysis or consult with a financial advisor before making an investment decision' in the answer. 
-            User question: {query}. You have the following information available about {company_name}. 
-            Write (5-8) point investment analysis to answer user query, At the end conclude with proper explanation. 
-            Try to Give positives and negatives: 
-            <stock_price>
-                {stock_data}
-            </stock_price> 
-            <stock_financials>
-                {stock_financials}
-            <stock_financials>
-            <stock_news>
-                {stock_news}
-            </stock_news>
-            Provide an analysis only base on the information provided above. Do not use any other external information or your own knowledge as the basis for the analysis.
-    """
+#     analysis_template = """
+#             Given detail stock analysis, Use the available data and provide investment recommendation.
+#             The user is fully aware about the investment risk, do not include any kind of warning like
+#             'It is recommended to conduct further research and analysis or consult with a financial advisor before making an investment decision' in the answer.
+#             User question: {query}. You have the following information available about {company_name}.
+#             Write (5-8) point investment analysis to answer user query, At the end conclude with proper explanation.
+#             Try to Give positives and negatives:
+#             <stock_price>
+#                 {stock_data}
+#             </stock_price>
+#             <stock_financials>
+#                 {stock_financials}
+#             <stock_financials>
+#             <stock_news>
+#                 {stock_news}
+#             </stock_news>
+#             Provide an analysis only base on the information provided above. Do not use any other external information or your own knowledge as the basis for the analysis.
+#     """
 
-    analysis_prompt = ChatPromptTemplate.from_template(analysis_template)
-    analysis_chain = analysis_prompt | llm | StrOutputParser()
+#     analysis_prompt = ChatPromptTemplate.from_template(analysis_template)
+#     analysis_chain = analysis_prompt | llm | StrOutputParser()
 
-    analysis = analysis_chain.invoke(
-        {
-            "query": query,
-            "company_name": company_name,
-            "stock_data": stock_data,
-            "stock_financials": stock_financials,
-            "stock_news": stock_news,
-        }
-    )
+#     analysis = analysis_chain.invoke(
+#         {
+#             "query": query,
+#             "company_name": company_name,
+#             "stock_data": stock_data,
+#             "stock_financials": stock_financials,
+#             "stock_news": stock_news,
+#         }
+#     )
 
-    return analysis
+#     return analysis
 
 
 if __name__ == "__main__":
     # llm = Ollama(model="llama3:70b")
     llm = Ollama(model="llama3")
     sql_chain = get_sql_chain(llm)
+    custom_tools = CustomTools(db_chain=sql_chain, llm=llm)
 
-    analyze_result = analyze_stock(
-        llm, "Is Tesla a good investment choice right now?", sql_chain)
-    logging.info(analyze_result)
+    tools = [
+        Tool(
+            name="get_stock_ticker",
+            func=custom_tools.get_stock_ticker,
+            description="Get the company stock ticker",
+        ),
+        Tool(
+            name="get_stock_price",
+            func=custom_tools.get_stock_price,
+            description="Use when you are asked to evaluate or analyze a stock. This will output historic share price data. You should input the the stock ticker to it "
+        ),
+        Tool(
+            name="get_recent_stock_news",
+            func=custom_tools.get_recent_stock_news,
+            description="Use this to fetch recent news about stocks"
+        ),
 
+        Tool(
+            name="get_financial_statements",
+            func=custom_tools.get_financial_statements,
+            description="Use this to get financial statement of the company. With the help of this data companys historic performance can be evaluaated. You should input stock ticker to it"
+        )
+
+    ]
+
+    # 3 -
+    agent_prompt_template = """
+        You are a financial advisor who has access to a set of tools that can help answer questions about stocks and investments.
+
+        You have access to the following tools:
+
+        {tools}
+
+        When you are done, respond with a final answer between <final_answer></final_answer>. 
+        For example:
+
+        <final_answer>Investment Analysis of Amazon.com, Inc. (AMZN)</final_answer>
+
+        Make sure you make use of all the tools available to you and not bias your answer based on an output from a single tool or your own knowledge.
+
+        Begin!
+
+        Question: {question}
+        """
+
+    agent_prompt = ChatPromptTemplate.from_template(
+        agent_prompt_template, partial_variables={"tools": tools})
+
+    agent_chain = (
+        {
+            "question": lambda x: x["input"],
+        }
+        | agent_prompt
+        | llm.bind(stop=["</final_answer>"])
+        | XMLAgentOutputParser()
+    )
+
+    agent_executor = AgentExecutor(
+        agent=agent_chain, tools=tools, verbose=True)
+    agent_response = agent_executor.invoke(
+        {"input": "Is Tesla a good investment choice right now?"})
+    logger.info(agent_response["output"])
+
+    # 2 -
+    # analyze_result = analyze_stock(
+    #     llm, "Is Tesla a good investment choice right now?", sql_chain)
+    # logging.info(analyze_result)
+
+    # 1 -
     # company_name, company_ticker = get_stock_ticker(
     #     "What is the main business of Amazon?", sql_chain)
     # logging.info("Company Name: %s", company_name)
--- util.py
+++ util.py
@@ -24,123 +24,192 @@ def extract_from_xml_tag(response: str, tag: str) -> str:
         return ""
 
 
-def get_stock_ticker(query, db_chain):
+def extract_docstring_info(docstring):
     """
-    Returns the ticker symbol and company name relevant to the given query.
+    Extracts the description, parameters, and their types and descriptions from a Google style docstring.
 
     Args:
-        query (str): The query to find the relevant ticker symbol and company name.
-        db_chain (Chain, optional): The database chain to use. Defaults to full_sql_chain.
+        docstring (str): The docstring to extract information from.
 
     Returns:
-        tuple: A tuple containing the company name and ticker symbol.
-
+        dict: A dictionary containing the description, parameters, and their types and descriptions.
     """
+    # Find the indices of "Args:" and "Returns:"
+    if (docstring is None):
+        return {"description": "Lambda", "params": []}
 
-    response = db_chain.invoke(
-        {
-            "question": f"Return the ticker symbol and company name that is relevant to this query {query}. Place symbol into <symbol>...</symbol> tags and company name into <company>...</company> tags."
-        }
+    args_index = docstring.index("Args:")
+
+    returns_index = docstring.index("Returns:")
+
+    # Extract the description
+    function_description = docstring[:args_index].strip()
+
+    # Extract the parameters
+    param_string = docstring[args_index + 6: returns_index].strip()
+    params = []
+    for param in param_string.split("\n"):
+        if param.strip() == "":
+            continue
+        param_name_type, description = param.strip().split(":")
+        param_name, param_type = param_name_type.split("(")
+        param_type = param_type[:-1]
+
+        params.append(
+            {
+                "name": param_name.strip(),
+                "type": param_type.strip(),
+                "description": description.strip(),
+            }
+        )
+
+    return {"description": function_description, "params": params}
+
+
+def construct_format_parameters_prompt(parameters):
+    constructed_prompt = "\n".join(
+        f"<parameter>\n<name>{parameter['name']}</name>\n<type>{parameter['type']
+                                                                }</type>\n<description>{parameter['description']}</description>\n</parameter>"
+        for parameter in parameters
     )
-    symbol = extract_from_xml_tag(response, "symbol")
-    company_name = extract_from_xml_tag(response, "company")
 
-    return company_name, symbol
+    return constructed_prompt
 
 
-def get_stock_price(ticker, history=10):
-    """
-    Returns the stock data for the specified ticker symbol.
-
-    Args:
-        ticker(str): The ticker symbol for which to fetch the stock data.
-        history(int): The number of days of historical data to fetch. Defaults to 10.
-
-    Returns:
-        tuple: A tuple containing the stock data and a unique name.
-
-    """
-
-    today = date.today()
-    start_date = today - timedelta(days=history)
-    data = pdr.get_data_yahoo(ticker, start=start_date, end=today)
-    dataname = ticker + "_" + str(today)
-
-    return data, dataname
+def construct_format_tool_prompt(name, description, parameters):
+    constructed_prompt = (
+        "<tool_description>\n"
+        f"<tool_name>{name}</tool_name>\n"
+        "<description>\n"
+        f"{description}\n"
+        "</description>\n"
+        "<parameters>\n"
+        f"{construct_format_parameters_prompt(parameters)}\n"
+        "</parameters>\n"
+        "</tool_description>"
+    )
+    return constructed_prompt
 
 
-# Fetch top 5 google news for given company name
-def google_query(search_term):
+class CustomTools:
+    def __init__(self, db_chain, llm) -> None:
+        self.db_chain = db_chain
+        self.llm = llm
 
-    if "news" not in search_term:
-        search_term = search_term + " stock news"
-    url = f"https://www.google.com/search?q={search_term}"
-    url = re.sub(r"\s", "+", url)
-    return url
+    def get_stock_ticker(self, query):
+        """
+        Returns the ticker symbol and company name relevant to the given query.
 
+        Args:
+            query (str): The query to find the relevant ticker symbol and company name.
+            db_chain (Chain): The database chain to use.
 
-def get_recent_stock_news(company_name):
-    """
-    Fetches and returns the top 5 recent news articles for a given company from Google News.
+        Returns:
+            tuple: A tuple containing the company name and ticker symbol.
 
-    Args:
-        company_name (str): The name of the company to fetch news for.
+        """
 
-    Returns:
-        str: A string containing the top 5 recent news articles for the company.
-    """
+        response = self.db_chain.invoke(
+            {
+                "question": f"Return the ticker symbol and company name that is relevant to this query {query}. Place symbol into <symbol>...</symbol> tags and company name into <company>...</company> tags."
+            }
+        )
+        symbol = extract_from_xml_tag(response, "symbol")
+        company_name = extract_from_xml_tag(response, "company")
 
-    headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36"
-    }
+        return company_name, symbol
 
-    g_query = google_query(company_name)
-    res = requests.get(g_query, headers=headers).text
-    soup = BeautifulSoup(res, "html.parser")
-    news = []
-    for n in soup.find_all("div", "n0jPhd ynAwRc tNxQIb nDgy9d"):
-        news.append(n.text)
-    for n in soup.find_all("div", "IJl0Z"):
-        news.append(n.text)
+    def get_stock_price(self, ticker, history=10):
+        """
+        Returns the stock data for the specified ticker symbol.
 
-    if len(news) > 6:
-        news = news[:4]
-    else:
-        news = news
-    news_string = ""
-    for i, n in enumerate(news):
-        news_string += f"{i}. {n}\n"
-    top5_news = "Recent News:\n\n" + news_string
+        Args:
+            ticker(str): The ticker symbol for which to fetch the stock data.
+            history(int): The number of days of historical data to fetch. Defaults to 10.
 
-    return top5_news
+        Returns:
+            tuple: A tuple containing the stock data and a unique name.
 
+        """
 
-def stock_news_search(company_name):
-    search = DuckDuckGoSearchRun()
-    return search("Stock news about " + company_name)
+        today = date.today()
+        start_date = today - timedelta(days=history)
+        data = pdr.get_data_yahoo(ticker, start=start_date, end=today)
+        dataname = ticker + "_" + str(today)
 
+        return data, dataname
 
-# Get financial statements from Yahoo Finance
-def get_financial_statements(ticker):
-    """
-    Fetches and returns the balance sheet for a given company ticker from Yahoo Finance.
+    # Fetch top 5 google news for given company name
 
-    Args:
-        ticker (str): The ticker of the company to fetch the balance sheet for.
+    def google_query(self, search_term):
 
-    Returns:
-        str: A string representation of the company's balance sheet.
-    """
+        if "news" not in search_term:
+            search_term = search_term + " stock news"
+        url = f"https://www.google.com/search?q={search_term}"
+        url = re.sub(r"\s", "+", url)
+        return url
 
-    if "." in ticker:
-        ticker = ticker.split(".")[0]
-    else:
-        ticker = ticker
-    company = yf.Ticker(ticker)
-    balance_sheet = company.balance_sheet
-    if balance_sheet.shape[1] >= 3:
-        # Only captures last 3 years of data
-        balance_sheet = balance_sheet.iloc[:, :3]
-    balance_sheet = balance_sheet.dropna(how="any")
-    balance_sheet = balance_sheet.to_string()
-    return balance_sheet
+    def get_recent_stock_news(self, company_name):
+        """
+        Fetches and returns the top 5 recent news articles for a given company from Google News.
+
+        Args:
+            company_name (str): The name of the company to fetch news for.
+
+        Returns:
+            str: A string containing the top 5 recent news articles for the company.
+        """
+
+        headers = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36"
+        }
+
+        g_query = self.google_query(company_name)
+        res = requests.get(g_query, headers=headers).text
+        soup = BeautifulSoup(res, "html.parser")
+        news = []
+        for n in soup.find_all("div", "n0jPhd ynAwRc tNxQIb nDgy9d"):
+            news.append(n.text)
+        for n in soup.find_all("div", "IJl0Z"):
+            news.append(n.text)
+
+        if len(news) > 6:
+            news = news[:4]
+        else:
+            news = news
+        news_string = ""
+        for i, n in enumerate(news):
+            news_string += f"{i}. {n}\n"
+        top5_news = "Recent News:\n\n" + news_string
+
+        return top5_news
+
+    def stock_news_search(self, company_name):
+        search = DuckDuckGoSearchRun()
+        return search("Stock news about " + company_name)
+
+    # Get financial statements from Yahoo Finance
+
+    def get_financial_statements(self, ticker):
+        """
+        Fetches and returns the balance sheet for a given company ticker from Yahoo Finance.
+
+        Args:
+            ticker (str): The ticker of the company to fetch the balance sheet for.
+
+        Returns:
+            str: A string representation of the company's balance sheet.
+        """
+
+        if "." in ticker:
+            ticker = ticker.split(".")[0]
+        else:
+            ticker = ticker
+        company = yf.Ticker(ticker)
+        balance_sheet = company.balance_sheet
+        if balance_sheet.shape[1] >= 3:
+            # Only captures last 3 years of data
+            balance_sheet = balance_sheet.iloc[:, :3]
+        balance_sheet = balance_sheet.dropna(how="any")
+        balance_sheet = balance_sheet.to_string()
+        return balance_sheet
