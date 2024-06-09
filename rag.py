5d80c5c5a2a93aa07c4c40902ca116cc0538a003
Shivaji Byrapaneni <Shivaji.byrapaneni@gmail.com>
2024-06-09 08:25:59 -0400

    RAG Working with history

--- rag.py
+++ rag.py
@@ -1,4 +1,5 @@
 """ Local RAG """
+import json
 import logging
 import sys
 import os
@@ -13,6 +14,12 @@ from langchain_community.llms import Ollama
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.runnables import RunnableParallel, RunnablePassthrough
+from operator import itemgetter
+from langchain.schema import format_document
+from langchain_core.messages import get_buffer_string
+from langchain_core.runnables import RunnableLambda
+from langchain.memory import ConversationBufferMemory
+from langchain.prompts.prompt import PromptTemplate
 
 
 from langchain_chroma import Chroma
@@ -79,7 +86,7 @@ if __name__ == "__main__":
 
     parentdoc_retriever.add_documents(load_files('.'), ids=None)
 
-    QUESTION = "what is a pod in kubernetes"
+    # QUESTION = "what is a pod in kubernetes"
     # QUESTION = "What are some acceptable model evaluation techniques?"
     # This should return keys to documents
     # logger.info(list(mem_store.yield_keys()))
@@ -96,24 +103,119 @@ if __name__ == "__main__":
 
     llm = Ollama(model="llama3")
 
-    LLM_PROMPT_TEMPLATE = """Answer the question based only on the following context. 
-        If the context does not provide sufficient information to answer the question, politely indicate that you are unable to assist. 
-        
-        {context}
-        
-        Question: {question}
-        """
-    prompt = ChatPromptTemplate.from_template(LLM_PROMPT_TEMPLATE)
-    output_parser = StrOutputParser()
+    # LLM_PROMPT_TEMPLATE = """Answer the question based only on the following context.
+    #     If the context does not provide sufficient information to answer the question, politely indicate that you are unable to assist.
 
-    # in the first step we retrieve the context and pass through the input question
-    setup_and_retrieval = RunnableParallel(
-        {"context": parentdoc_retriever, "question": RunnablePassthrough()}
+    #     {context}
 
-    )
+    #     Question: {question}
+    #     """
+    # prompt = ChatPromptTemplate.from_template(LLM_PROMPT_TEMPLATE)
+    # output_parser = StrOutputParser()
 
-    # In the subsequent steps pass the context and question to the prompt, send the prompt to the llm and parse the output as a string
-    chain = setup_and_retrieval | prompt | llm | output_parser
+    # # in the first step we retrieve the context and pass through the input question
+    # setup_and_retrieval = RunnableParallel(
+    #     {"context": parentdoc_retriever, "question": RunnablePassthrough()}
 
-    response = chain.invoke(QUESTION)
-    logger.info(response)
+    # )
+
+    # # In the subsequent steps pass the context and question to the prompt, send the prompt to the llm and parse the output as a string
+    # chain = setup_and_retrieval | prompt | llm | output_parser
+
+    # response = chain.invoke(QUESTION)
+    # logger.info(response)
+
+    # template to rephrase the question
+    condense_template = """Given the following conversation and a follow up question, rephrase the follow up question to be a standalone question.
+    Skip any preamble or summarization, simply generate the rephrased question.
+
+    {chat_history}
+    
+    Follow Up Input: {question}
+    """
+    CONDENSE_QUESTION_PROMPT = PromptTemplate.from_template(condense_template)
+
+    # template to generate a response
+    answer_template = """You are a foremost expert in model risk and model governance. Your job is to advise users on the best practices and guidelines in these areas.
+    The context below provides relevant information to answer the question. You must use this context to provide a detailed and accurate response.
+    Only answer questions related to model risk and model governance, if a user asks a question about a different topic, politely decline.
+
+    {context}
+
+    Question: {question}
+    """
+    ANSWER_PROMPT = ChatPromptTemplate.from_template(answer_template)
+
+    # instantiate a blank memory buffer
+    memory = ConversationBufferMemory(
+        return_messages=True, output_key="answer", input_key="question")
+
+    # First we add a step to load memory from the buffer to feed into the prompt
+    loaded_memory = RunnablePassthrough.assign(chat_history=RunnableLambda(
+        memory.load_memory_variables) | itemgetter("history"),)
+
+    # Next we generate the standalone question
+    standalone_question = {
+        "standalone_question": {
+            "question": lambda x: x["question"],
+            "chat_history": lambda x: get_buffer_string(
+                x["chat_history"], human_prefix="human", ai_prefix="assistant"
+            ),
+        }
+        | CONDENSE_QUESTION_PROMPT
+        | llm
+        | StrOutputParser(),
+    }
+    # Retrieve the documents using the generated question
+    retrieved_documents = {
+        "docs": itemgetter("standalone_question") | parentdoc_retriever,
+        "question": lambda x: x["standalone_question"],
+    }
+    # Construct the inputs for the final prompt with the formatted context docs
+    # template and function to format context documents
+    DEFAULT_DOCUMENT_PROMPT = PromptTemplate.from_template(
+        template="{page_content}")
+
+    final_inputs = {
+        "context": lambda x: [format_document(doc, DEFAULT_DOCUMENT_PROMPT) for doc in x["docs"]],
+        "question": itemgetter("question"),
+    }
+    # Send the final prompt to the llm
+    answer = {
+        "answer": final_inputs | ANSWER_PROMPT | llm,
+        "docs": itemgetter("docs"),
+    }
+    # And now we put it all together!
+    final_chain = loaded_memory | standalone_question | retrieved_documents | answer
+
+    logger.info(
+        "Question:: What types of model risks should be documented? \n\n")
+    inputs = {"question": "What types of model risks should be documented?"}
+    result = final_chain.invoke(inputs)
+    logger.info(result["answer"])
+    logger.info("source documents:%s \n", json.dumps(
+        [doc.metadata for doc in result["docs"]], indent=2))
+
+    # after every conversation turn we update the conversation state in the memory buffer
+    memory.save_context(inputs, {"answer": result["answer"]})
+
+    logger.info(
+        "Question:: Can you provide some examples for a fraud detection model? \n\n")
+    inputs = {
+        "question": "Can you provide some examples for a fraud detection model?"}
+    result = final_chain.invoke(inputs)
+    logger.info(result["answer"])
+    logger.info("source documents: %s\n", json.dumps(
+        [doc.metadata for doc in result["docs"]], indent=2))
+
+    memory.save_context(inputs, {"answer": result["answer"]})
+
+    logger.info("Question:: How about for a credit scoring model? \n\n")
+
+    inputs = {"question": "How about for a credit scoring model?"}
+    result = final_chain.invoke(inputs)
+    logger.info(result["answer"])
+    logger.info("source documents: %s\n", json.dumps(
+        [doc.metadata for doc in result["docs"]], indent=2))
+
+    memory.save_context(inputs, {"answer": result["answer"]})
