87c64ffe332a9e03b07b64359fa1c881af9d0627
Shivaji Byrapaneni <Shivaji.byrapaneni@gmail.com>
2024-06-09 07:18:48 -0400

    RAG Retriever Working

--- rag.py
+++ rag.py
@@ -1,16 +1,19 @@
 """ Local RAG """
 import logging
-import re
 import sys
-from pathlib import Path
+import os
+import glob
 from langchain.vectorstores import Chroma
 from langchain_community.embeddings import OllamaEmbeddings
-from langchain_community.document_loaders import RecursiveUrlLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from bs4 import BeautifulSoup as Soup
-from langchain.document_loaders import PyPDFLoader
-from pdf_utils import PyPDFOutlineParser
+from langchain.document_loaders import PyPDFLoader, TextLoader
+from langchain.storage import InMemoryStore
+from langchain.retrievers import ParentDocumentRetriever
 from langchain_community.llms import Ollama
+
+
+from langchain_chroma import Chroma
+from chromadb.config import (Settings)
 
 # Logger Setup
 logger = logging.getLogger(__name__)
@@ -18,81 +21,69 @@ logging.basicConfig(stream=sys.stdout, level=logging.INFO,
                     format='%(asctime)s - %(levelname)s - %(message)s')
 # Constants
 OLLAMA_LLM3_MODEL = "llama3"
-PREFIXES_TO_IGNORE = ("javascript:", "mailto:", "#")
-SUFFIXES_TO_IGNORE = (
-    ".css",
-    ".js",
-    ".ico",
-    ".png",
-    ".jpg",
-    ".jpeg",
-    ".gif",
-    ".svg",
-    ".csv",
-    ".bz2",
-    ".zip",
-    ".epub",
-)
-SUFFIXES_TO_IGNORE_REGEX = (
-    "(?!" + "|".join([re.escape(s) +
-                      r"[\#'\"]" for s in SUFFIXES_TO_IGNORE]) + ")"
-)
-PREFIXES_TO_IGNORE_REGEX = (
-    "(?!" + "|".join([re.escape(s) for s in PREFIXES_TO_IGNORE]) + ")"
-)
 
 
-def scrape_url(url):
-    """Scrapes a given URL and returns a vector of Texts"""
-    logger.info("Loading URL [%s]", url)
-    # scrape data from web
-    docs = RecursiveUrlLoader(
-        url,
-        max_depth=4,
-        extractor=lambda x: Soup(x, "html.parser").text,
-        prevent_outside=True,
-        use_async=True,
-        timeout=600,
-        check_response_status=True,
-        # drop trailing / to avoid duplicate pages.
-        link_regex=(
-            f"href=[\"']{
-                PREFIXES_TO_IGNORE_REGEX}((?:{SUFFIXES_TO_IGNORE_REGEX}.)*?)"
-            r"(?:[\#'\"]|\/[\#'\"])"
-        ),
-    ).load()
+def load_files(folder_path):
+    md_files = []
+    pdf_files = []
 
-    # split text
-    # this chunk_size and chunk_overlap effects to the prompt size
-    # execeed promt size causes error `prompt size exceeds the context window size and cannot be processed`
-    text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size=1000, chunk_overlap=200)
-    docs = text_splitter.split_documents(docs)
-    # docs_as_txt = list(map(lambda doc: doc.page_content, docs))
-    # return docs_as_txt
-    return docs[:5]
+    # files = os.listdir(folder_path)
+    files = glob.glob(folder_path+"/**/*.*", recursive=True)
 
+    for file in files:
+        if file.endswith('.md'):
+            md_files.append(file)
+        elif file.endswith('.pdf'):
+            pdf_files.append(file)
 
-def load_pdf():
-    pdf_files = list(Path(".").glob("**/*.pdf"))
-    section_chunks_local = []
+    logger.info('MD Files: %s', md_files)
+    logger.info('PDF Files: %s', pdf_files)
 
-    for pdf in pdf_files:
-        loader = PyPDFLoader(file_path=pdf.as_posix())
-        loader.parser = PyPDFOutlineParser()
-        sections = loader.load()
-        for sec in sections:
-            sec.metadata.update({"file": pdf.name})
+    loaders = [TextLoader(os.path.join(folder_path, file))
+               for file in md_files]
+    loaders.extend([PyPDFLoader(os.path.join(folder_path, file))
+                    for file in pdf_files])
+    tmp_docs = []
+    for loader in loaders:
+        tmp_docs.extend(loader.load())
 
-        section_chunks_local += sections
-    return section_chunks_local
+    return tmp_docs
 
 
 if __name__ == "__main__":
-    section_chunks = load_pdf()
-    embeddings = OllamaEmbeddings(model="nomic-embed-text")
-    chrome_vectordb = Chroma.from_documents(
-        documents=section_chunks, embedding=embeddings, persist_directory="./chromadb/data")
-    chrome_vectordb.persist()
+    # https://python.langchain.com/v0.1/docs/modules/data_connection/retrievers/parent_document_retriever/
+    chrome_vectordb = Chroma(
+        collection_name="documents", embedding_function=OllamaEmbeddings(model="nomic-embed-text"),
+        client_settings=Settings(allow_reset=False, anonymized_telemetry=False,
+                                 persist_directory="./chromadb/data")
+    )
+    # chrome_vectordb = Chroma.from_documents(
+    #     documents=docs, embedding=OllamaEmbeddings(model="nomic-embed-text"), persist_directory="./chromadb/data")
+    # chrome_vectordb.persist()
+
+    child_splitter = RecursiveCharacterTextSplitter(chunk_size=400)
+    # parent_splitter = RecursiveCharacterTextSplitter(chunk_size=2000)
+    # Parent docs are being saved in memory
+    mem_store = InMemoryStore()
+
+    parentdoc_retriever = ParentDocumentRetriever(
+        vectorstore=chrome_vectordb,
+        docstore=mem_store,
+        child_splitter=child_splitter,
+        # parent_splitter=parent_splitter
+    )
+    # logger.info(docs)
+
+    parentdoc_retriever.add_documents(load_files('.'), ids=None)
+
+    # This should return keys to documents
+    # logger.info(list(mem_store.yield_keys()))
+    # This should return chunks from Chroma Vector DB
+    logger.info(chrome_vectordb.similarity_search(
+        "what is a pod in kubernetes"))
+    # This should get Main document
+    parentdoc_retriever.search_kwargs = {"k": 2}
+    logger.info(parentdoc_retriever.invoke(
+        "what is a pod in kubernetes"))
 
     llm = Ollama(model="llama3")
